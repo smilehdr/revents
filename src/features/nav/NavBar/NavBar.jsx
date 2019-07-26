4b7268f8196d0cc17c0a9decc4796b84f8ba088e
Hendra <smilehdr@gmail.com>
2019-07-26 09:34:30 +0700

    End of Section 11

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,40 +1,76 @@
-import React, { Component } from 'react'
-import {Menu, Container, Button} from 'semantic-ui-react'
-import { NavLink, Link, withRouter } from 'react-router-dom';
-import SignedOutMenu from '../../nav/Menus/SignedOutMenu';
-import SignedInMenu from '../../nav/Menus/SignedInMenu';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+import SignedOutMenu from "../../nav/Menus/SignedOutMenu";
+import SignedInMenu from "../../nav/Menus/SignedInMenu";
+import { openModal } from "../../modals/modalActions";
+import {logout} from '../../auth/authActions';
+
+const actions = {
+  openModal,
+  logout
+};
+
+const mapState = (state) => ({
+  auth: state.auth
+})
 
 class NavBar extends Component {
-    state = {
-        authenticated: false
-    }
 
-    handleSignIn = () => this.setState({authenticated: true});
-    handleSignOut = () => {
-        this.setState({authenticated: false});
-        this.props.history.push('/');
-    };
+  handleSignIn = () => {
+    this.props.openModal("LoginModal");
+  };
 
-    render() {
-        const {authenticated} = this.state;
-        return (
-            <Menu inverted fixed="top">
-                <Container>
-                    <Menu.Item as ={NavLink} exact to="/" header>
-                        <img src="/assets/logo.png" alt="logo" />
-                        Re-vents
-                    </Menu.Item>
-                    <Menu.Item as={NavLink} exact to="/events" name="Events" />
-                    <Menu.Item as={NavLink} to="/people" name="People" />
-                    <Menu.Item as={NavLink} to="/test" name="Test" />
-                    <Menu.Item>
-                        <Button  as={Link} to="/createEvent" floated="right" positive inverted content="Create Event" />
-                    </Menu.Item>
-                    {authenticated ? <SignedInMenu signOut={this.handleSignOut} /> : <SignedOutMenu signIn={this.handleSignIn}/>}
-                </Container>
-            </Menu>
-        )
-    }
+  handleRegister = () => {
+    this.props.openModal("RegisterModal");
+  };
+
+  handleSignOut = () => {
+    this.props.logout();
+    this.props.history.push("/");
+  };
+
+  render() {
+    const { auth } = this.props;
+    const authenticated = auth.authenticated;
+    return (
+      <Menu inverted fixed="top">
+        <Container>
+          <Menu.Item as={NavLink} exact to="/" header>
+            <img src="/assets/logo.png" alt="logo" />
+            Re-vents
+          </Menu.Item>
+          <Menu.Item as={NavLink} exact to="/events" name="Events" />
+          <Menu.Item as={NavLink} to="/people" name="People" />
+          <Menu.Item as={NavLink} to="/test" name="Test" />
+          <Menu.Item>
+            <Button
+              as={Link}
+              to="/createEvent"
+              floated="right"
+              positive
+              inverted
+              content="Create Event"
+            />
+          </Menu.Item>
+          {authenticated ? (
+            <SignedInMenu signOut={this.handleSignOut} currentUser= {auth.currentUser} />
+          ) : (
+            <SignedOutMenu
+              signIn={this.handleSignIn}
+              register={this.handleRegister}
+            />
+          )}
+        </Container>
+      </Menu>
+    );
+  }
 }
 
-export default withRouter(NavBar); 
+export default withRouter(
+  connect(
+    mapState,
+    actions
+  )(NavBar)
+);
--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -5,6 +5,7 @@ import { Button } from "semantic-ui-react";
 import TestPlaceInput from "./TestPlaceInput";
 import SimpleMap from "./SimpleMap";
 import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
+import { openModal } from "../modals/modalActions";
 
 const mapState = state => ({
   data: state.test.data
@@ -12,14 +13,15 @@ const mapState = state => ({
 
 const actions = {
   incrementCounter,
-  decrementCounter
+  decrementCounter,
+  openModal
 };
 
 class TestComponent extends Component {
   state = {
     latlng: {
-        lat: 59.95,
-        lng: 30.33
+      lat: 59.95,
+      lng: 30.33
     }
   };
 
@@ -27,23 +29,28 @@ class TestComponent extends Component {
     geocodeByAddress(address)
       .then(results => getLatLng(results[0]))
       .then(latLng => {
-          this.setState({
-            latlng: latLng
-          })
+        this.setState({
+          latlng: latLng
+        });
       })
       .catch(error => console.error("Error", error));
   };
 
   render() {
-    const { data, incrementCounter, decrementCounter } = this.props;
+    const { data, incrementCounter, decrementCounter, openModal } = this.props;
     return (
       <div>
         <h1>Test Component</h1>
         <h3>The answer is {data}</h3>
         <Button onClick={incrementCounter} positive content="Increment" />
         <Button onClick={decrementCounter} negative content="Decrement" />
-        <TestPlaceInput selectAddress ={this.handleSelect}/>
-        <SimpleMap key={this.state.latlng.lng} latlng= {this.state.latlng}/>
+        <Button
+          onClick={() => openModal("TestModal", { data: 42 })}
+          color="teal"
+          content="Open Modal"
+        />
+        <TestPlaceInput selectAddress={this.handleSelect} />
+        <SimpleMap key={this.state.latlng.lng} latlng={this.state.latlng} />
       </div>
     );
   }
