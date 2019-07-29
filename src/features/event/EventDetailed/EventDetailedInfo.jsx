e90c1ec63b98f951767089684e42ef9b5adb92b4
Hendra <smilehdr@gmail.com>
2019-07-29 10:19:27 +0700

    End of Section 13

--- src/features/event/EventDetailed/EventDetailedInfo.jsx
+++ src/features/event/EventDetailed/EventDetailedInfo.jsx
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Segment, Grid, Icon, Button } from "semantic-ui-react";
 import EventDetailedMap from "./EventDetailedMap";
+import { format, parseISO } from "date-fns";
 
-const EventDetailedInfo = ({event}) => {
+const EventDetailedInfo = ({ event }) => {
   const [isMapOpen, showMapToggle] = useState(false);
   return (
     <Segment.Group>
@@ -22,7 +23,12 @@ const EventDetailedInfo = ({event}) => {
             <Icon name="calendar" size="large" color="teal" />
           </Grid.Column>
           <Grid.Column width={15}>
-            <span>{event.date}</span>
+            {event.date && (
+              <span>
+                {format(parseISO(event.date), "EEEE do LLL")} at{" "}
+                {format(parseISO(event.date), "h:mm a")}
+              </span>
+            )}
           </Grid.Column>
         </Grid>
       </Segment>
@@ -35,11 +41,21 @@ const EventDetailedInfo = ({event}) => {
             <span>{event.venue}</span>
           </Grid.Column>
           <Grid.Column width={4}>
-            <Button onClick={() => showMapToggle(!isMapOpen)} color="teal" size="tiny" content={isMapOpen ? 'Hide Map' : 'Open Map'} />
+            <Button
+              onClick={() => showMapToggle(!isMapOpen)}
+              color="teal"
+              size="tiny"
+              content={isMapOpen ? "Hide Map" : "Open Map"}
+            />
           </Grid.Column>
         </Grid>
       </Segment>
-      {isMapOpen && <EventDetailedMap lat = {event.venueLatLng.lat} lng = {event.venueLatLng.lng}/>}
+      {isMapOpen && (
+        <EventDetailedMap
+          lat={event.venueLatLng.lat}
+          lng={event.venueLatLng.lng}
+        />
+      )}
     </Segment.Group>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,36 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
-import {Provider} from 'react-redux';
-import { configureStore } from './app/store/configureStore';
-import ScrollToTop from './app/common/util/ScrollToTop';
-import { loadEvents } from './features/event/eventActions';
+import React from "react";
+import ReactDOM from "react-dom";
+import "react-redux-toastr/lib/css/react-redux-toastr.min.css";
+import "./index.css";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import ReduxToastr from "react-redux-toastr";
+import { configureStore } from "./app/store/configureStore";
+import ScrollToTop from "./app/common/util/ScrollToTop";
+import { loadEvents } from "./features/event/eventActions";
 
 const store = configureStore();
 store.dispatch(loadEvents());
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 
 let render = () => {
-    ReactDOM.render(
-        <Provider store= {store}>
-            <BrowserRouter>
-                <ScrollToTop>
-                    <App />
-                </ScrollToTop>
-            </BrowserRouter>
-        </Provider>
-        
-        ,rootEl
-    );
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop>
+          <ReduxToastr
+            position="bottom-right"
+            transitionIn="fadeIn"
+            transitionOut="fadeOut"
+          />
+          <App />
+        </ScrollToTop>
+      </BrowserRouter>
+    </Provider>,
+
+    rootEl
+  );
 };
 
-if(module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render);
-    });
+if (module.hot) {
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 
 render();
